c6ccda846a93c4d1a52dc513f594d55e50ad712c
ronyeo223 <101795068+ronyeo223@users.noreply.github.com>
2022-03-22 17:40:43 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
+
 from flask import Flask, render_template
 import os
+import socket
 
 ip_list = ['192.168.229.158']
 app = Flask(__name__)
+
+s =socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.connect((socket.gethostname(), 1024))
+
+
 
 @app.route('/')
 def index():
@@ -10,21 +18,24 @@ def index():
 
 @app.route('/my-link/', methods = ['POST', 'GET'])
 def my_link():
-        response = os.system('ping -n 4 192.168.229.158')
-        if "Received = 4":
+  while True:
+     response = os.system('ping -n 4 192.168.229.158')
+     if "Received = 4":
             print(f"UP 192.168.229.158 Ping Successful")
-
+            msg = s.recv(1024)
+            print(msg.decode("utf-8"))
             return 'Ping Successful'
-        else:
+
+     else:
             print(f"DOWN 192.168.229.158 Ping Unsuccessful")  
 
             return 'Ping Unsuccessful'
+
+     
+
+
         
 
 if __name__ == '__main__':
   app.debug = True
   app.run(host="0.0.0.0")
-
-        
-
-
