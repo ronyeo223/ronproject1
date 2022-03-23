f4353b52819dd142ea2f138861329f729135a9a0
ronyeo223 <101795068+ronyeo223@users.noreply.github.com>
2022-03-23 16:14:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 import os
 import socket
 
-ip_list = ['192.168.229.158']
+ip_list = ['192.168.229.170']
 app = Flask(__name__)
 
 s =socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -20,12 +20,12 @@ def index():
 @app.route('/my-link/')
 def my_link():
   while True:
+     clientsocket, address = s.accept()
+     print(f"connection from {address} has been established!")
+     clientsocket.send(bytes("welcome to the server", "utf-8"))
      response = os.system('ping -n 4 192.168.229.170')
      if "Received = 4":
             print(f"UP 192.168.229.170 Ping Successful")
-            clientsocket, address = s.accept()
-            print(f"connection from {address} has been established!")
-            clientsocket.send(bytes("welcome to the server", "utf-8"))
             return 'Ping Successful'
 
      else:
