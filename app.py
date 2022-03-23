d62907e102d905fec9f86d941b406d1b76bd6eb2
ronyeo223 <101795068+ronyeo223@users.noreply.github.com>
2022-03-23 13:59:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ app = Flask(__name__)
 
 s =socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-s.connect(('192.168.229.170', 1024))
+s.bind(("192.168.229.170", 1024))
+s.listen(5)
 
 
 
@@ -22,15 +23,22 @@ def my_link():
      response = os.system('ping -n 4 192.168.229.170')
      if "Received = 4":
             print(f"UP 192.168.229.170 Ping Successful")
-            msg = s.recv(1024)
-            print(msg.decode("utf-8"))
+            clientsocket, address = s.accept()
+            print(f"connection from {address} has been established!")
+            clientsocket.send(bytes("welcome to the server", "utf-8"))
             return 'Ping Successful'
 
      else:
             print(f"DOWN 192.168.229.170 Ping Unsuccessful")  
 
             return 'Ping Unsuccessful'
+        
 
+if __name__ == '__main__':
+  app.debug = True
+  app.run(host="0.0.0.0")
+
+        
      
 
 
