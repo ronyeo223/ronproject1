df99147fac76948864f05a071d8b789e518b7c3d
ronyeo223 <101795068+ronyeo223@users.noreply.github.com>
2022-03-24 11:25:36 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 
+from http import client
 from flask import Flask, render_template
+from flask_socketio import socketio, emit
 import os
 import socket
+
 
 ip_list = ['192.168.229.170']
 app = Flask(__name__)
@@ -10,21 +13,17 @@ s =socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 hostname = socket.gethostname()
 dns_resolved_addr = socket.gethostbyname(hostname)
-s.bind((dns_resolved_addr, 5001))
+s.bind((socket.gethostname(), 5000))
 s.listen(5)
-
 
 
 @app.route('/')
 def index():
-  return render_template('index.html')
+   return render_template('index.html')
 
 @app.route('/my-link/')
 def my_link():
-  while True:
-     clientsocket, address = s.accept()
-     print(f"connection from {address} has been established!")
-     clientsocket.send(bytes("welcome to the server", "utf-8"))
+
      response = os.system('ping -n 4 192.168.229.170')
      if "Received = 4":
             print(f"UP 192.168.229.170 Ping Successful")
@@ -34,8 +33,14 @@ def my_link():
             print(f"DOWN 192.168.229.170 Ping Unsuccessful")  
 
             return 'Ping Unsuccessful'
-        
 
+@socketio.event
+def pingevent(ping):
+       socketio.emit('web_BBBWEvent', ping)
+
+       
 if __name__ == '__main__':
   app.debug = True
-  app.run(host="0.0.0.0", port=5001)
+  app.run(host="0.0.0.0", port = 5000)
+
+        
