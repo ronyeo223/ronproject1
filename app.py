3d22d6f4f301889fac2da155df8908340c7e7033
ronyeo223 <101795068+ronyeo223@users.noreply.github.com>
2022-03-25 15:56:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,43 +1,40 @@
-
-from http import client
-from flask import Flask, render_template
-from flask_socketio import socketio, emit
+import asyncio
+import websockets
+from flask import Flask, render_template, request
 import os
-import socket
 
 
 ip_list = ['192.168.229.170']
 app = Flask(__name__)
-
-s =socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-s.bind(('172.20.10.2', 5000))
-s.listen(5)
-
+async def handler(websocket, path):
+ 
+    data = await websocket.recv()
+    reply = f"Data recieved as:  {data}!"
+    await websocket.send(reply)
 
 @app.route('/')
 def index():
    return render_template('index.html')
 
 @app.route('/my-link/')
-def my_link():
+async def my_link():
+       response = os.system('ping -n 4 192.168.229.170')
+       if "Received = 4":
+                start_server = websockets.serve(handler, "localhost", 5000)
+                await asyncio.Future()
+                print(f"UP 192.168.229.170 Ping Successful")
+                return 'Ping Successful'
 
-     response = os.system('ping -n 4 172.20.10.2')
-     if "Received = 4":
-            print(f"UP 192.168.229.170 Ping Successful")
-            return 'Ping Successful'
+       else:
+                print(f"DOWN 192.168.229.170 Ping Unsuccessful")  
+                return 'Ping Unsuccessful'
 
-     else:
-            print(f"DOWN 192.168.229.170 Ping Unsuccessful")  
 
-            return 'Ping Unsuccessful'
 
-@socketio.event
-def pingevent(ping):
-       socketio.emit('web_BBBWEvent', ping)
-
-       
 if __name__ == '__main__':
   app.debug = True
-  socketio.run(app)
+  app.run(host="0.0.0.0")
+
+
         
+
