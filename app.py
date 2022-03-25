ae486c26fbe5f63bd5a974abf29e1eac8db2bbba
ronyeo223 <101795068+ronyeo223@users.noreply.github.com>
2022-03-25 16:52:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,39 @@
 import asyncio
-import signal
+import websockets
+from flask import Flask, render_template, request
 import os
 
-import websockets
+
+ip_list = ['192.168.229.170']
+app = Flask(__name__)
+async def handler(websocket, path):
+ 
+    data = await websocket.recv()
+    reply = f"Data recieved as:  {data}!"
+    await websocket.send(reply)
+
+@app.route('/')
+def index():
+   return render_template('index.html')
+
+@app.route('/my-link/', methods = ["POST", "GET"])
+async def my_link():
+       response = os.system('ping -n 4 192.168.229.170')
+       if "Received = 4":
+                start_server = websockets.serve(handler, "192.168.229.170", 5000)
+                await asyncio.Future()
+                print(f"UP 192.168.229.170 Ping Successful")
+                return 'Ping Successful'
+
+       else:
+                print(f"DOWN 192.168.229.170 Ping Unsuccessful")  
+                return 'Ping Unsuccessful'
 
 
-async def echo(websocket):
-    async for message in websocket:
-        await websocket.send(message)
+
+if __name__ == '__main__':
+  app.debug = True
+  app.run(host="0.0.0.0")
 
 
-async def main():
-    # Set the stop condition when receiving SIGTERM.
-    loop = asyncio.get_running_loop()
-    stop = loop.create_future()
-    loop.add_signal_handler(signal.SIGTERM, stop.set_result, None)
-
-    async with websockets.serve(
-        echo,
-        host="192.168.229.170",
-        port=int(os.environ["8000"]),
-    ):
-        await stop
-
-
-if __name__ == "__main__":
-    asyncio.run(main())
-
-        
 
