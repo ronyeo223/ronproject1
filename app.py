59c2c43e9704daada45d26893f7e554a0afc03ac
ronyeo223 <101795068+ronyeo223@users.noreply.github.com>
2022-03-29 14:52:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 
+from pythonping import ping
 from flask import Flask, render_template, request
 import os
 import asyncio
@@ -6,16 +7,16 @@ from websockets import serve
 
 app = Flask(__name__)
 
-
+ip_list = ["192.168.229.170"]
 
 @app.route('/')
-async def index():
+def index():
    return render_template('index.html')
 
 @app.route('/my-link/', methods = ["POST","GET"])
-async def my_link():
-       response = os.system('ping -n 4 192.168.229.170')
-       if "Received = 4":
+def my_link():
+       response = os.system(f'ping -n 4 192.168.229.170')
+       if response == 0:
                 print(f"UP 192.168.229.170 Ping Successful")
                 
                 return 'Ping Successful'
@@ -23,7 +24,6 @@ async def my_link():
        else:
                 print(f"DOWN 192.168.229.170 Ping Unsuccessful")  
                 return 'Ping Unsuccessful'
-
 
 if __name__ == '__main__':
   app.debug = True
