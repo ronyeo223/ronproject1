80325c9fbdeb50934036deea0265d76df7448d23
ronyeo223 <101795068+ronyeo223@users.noreply.github.com>
2022-04-01 16:29:42 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,14 +3,13 @@ import paho.mqtt.client as mqtt
 
 app = Flask(__name__)
 ip_list = {"addr": "192.168.229.170"}
-
+client = mqtt.Client()
 @app.route('/')
 def index():
    return render_template('index.html')
 
 @app.route('/mylink/<string:ip_list>', methods = ["GET"])
 def my_link(ip_list):
-   client = mqtt.Client()
    client.connect_async("driver.cloudmqtt.com", 18626)
    return {"pinged": ip_list}
 
