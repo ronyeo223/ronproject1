4b461b6389630af0afc2c549f58d1cf4e73ad3b4
ronyeo223 <101795068+ronyeo223@users.noreply.github.com>
2022-04-05 11:03:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template
 import paho.mqtt.client as mqtt
-import time
 
 app = Flask(__name__)
-ip_list = {"addr": "192.168.229.170"}
+
+direction = {"dire1" : "Forward",
+             "dire2" : "Left",
+             "dire3" : "Back",
+             "dire4" : "Right"}
+
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
@@ -20,6 +24,7 @@ client = mqtt.Client()
 
 @app.route('/')
 def index():
+
    client.on_connect = on_connect
    client.on_message = on_message
    client.on_publish = on_publish 
@@ -27,14 +32,27 @@ def index():
    client.connect("driver.cloudmqtt.com", 18626, 60)
    return render_template('index.html')
 
-@app.route('/mylink/<string:ip_list>', methods = ["GET"])
-def my_link(ip_list):
+@app.route('/mylink/<string:dire>', methods = ["GET"])
+def my_link1(dire):
    client.loop_start
-   client.publish("$SYS/ping", "Hello World", qos = 1)
+   if dire == "dire1":
+      client.publish("$SYS/direction", "Forward", qos = 1)
+   
+   elif dire == "dire2":
+      client.publish("$SYS/direction", "Left", qos = 1)
+
+   elif dire == "dire3":
+      client.publish("$SYS/direction", "Back", qos = 1)
+
+   elif dire == "dire4":
+      client.publish("$SYS/direction", "Right", qos = 1)
+
    client.loop_stop
-   return {"pinged": ip_list}
+   return direction[dire]
+
 
 
 
 if __name__ == '__main__':
    app.run(debug = True, host = "0.0.0.0")
+
